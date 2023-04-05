f108701618de4f5fc8415b30ad4a22615511e9f4
Ulrich Bittner <ubittner@users.noreply.github.com>
2023-04-05 12:50:44 +0200

    added pdf library

---
+++ libs/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5e48a63a2b0922cd9d1f1b239369d265719bf161',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5e48a63a2b0922cd9d1f1b239369d265719bf161',
+    ),
+    'tecnickcom/tcpdf' => 
+    array (
+      'pretty_version' => '6.3.5',
+      'version' => '6.3.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '19a535eaa7fb1c1cac499109deeb1a7a201b4549',
+    ),
+  ),
+);
